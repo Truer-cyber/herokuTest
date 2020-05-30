80c5dd12627e99758cdbbc077fda2f832327820f
Truer-cyber <66049924+Truer-cyber@users.noreply.github.com>
2020-05-30 17:38:56 -0500

    Update app.js

--- app.js
+++ app.js
@@ -4,7 +4,12 @@ const port = process.env.PORT || 3000
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/html');
-  res.end('<h1>DER MUNGOREICH VILL RISE AGAIN!</h1>');
+  res.end('
+          <head>
+              <title>RISEN 8192</title>
+          </head>
+          <h1>SOON.</h1>
+          ');
 });
 
 server.listen(port,() => {
