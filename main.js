1fbf1534066f28180e44bf723f730e21b4fba751
Truer-cyber <66049924+Truer-cyber@users.noreply.github.com>
2020-05-31 03:40:03 -0500

    Update main.js

--- main.js
+++ main.js
@@ -133,14 +133,14 @@ io.on('connection', socket => {
 	// socket.emit => to one client
 	// io.emit => to all clients
 	// 
-	let user = getUser(socket.handshake.address);
+	/*let user = getUser(socket.handshake.address);
 	socket.emit('canvas', {canvas:canvas, chargeMax:chargeMax,
 		addr: socket.handshake.address,
 		endpt: socket.manager.handshaken[socket.id].address,
 		ra: socket.request.connection.remoteAddress,
 		colors:colors.slice(0,Math.floor(Math.sqrt(user.EXP)+1))});
 	// disconnections
-	/*socket.on('disconnect', () => {
+	socket.on('disconnect', () => {
 		console.log("Connection lost.");
 	});*/
 });
